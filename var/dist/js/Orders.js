7a574e2612242d8c469cabdc51aeff555ac7db2f
kavanasril92 <76195415+kavanasril92@users.noreply.github.com>
2023-07-16 16:31:43 +0800

    Add files via upload

--- var/dist/js/Orders.js
+++ var/dist/js/Orders.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+	$( "#ordersForm" ).on( "submit", function( event ) {
+	  var outletVar = $(".outlet-select").find(":selected").val();
+	  
+	  if ( outletVar == "" || typeof outletVar == "undefined" ) {
+		alert("Please input outlet");
+		$(".outlet-select").focus();
+		event.preventDefault();		  
+	  }
+	});
+	
 });
