1496c5efd681d95a02ada4269075f7fc46ebe3f3
kavanasril92 <76195415+kavanasril92@users.noreply.github.com>
2023-07-18 21:30:36 +0800

    Add files via upload

--- var/dist/js/Orders.js
+++ var/dist/js/Orders.js
@@ -16,12 +16,30 @@ $( document ).ready(function() {
 		  
 		  $('#RWS_Accordion').find('input[type=number]').val('');
 	  } else if ( $(this).val() == "RWS" ) {
-		  $("#RWS_Accordion").show();
 		  $("#Amoy_Accordion").hide();
+		  $("#RWS_Accordion").show();
 		  
 		  $('#Amoy_Accordion').find('input[type=number]').val('');
 		  // $('#flush-collapseSixAmoy').collapse();
 	  }
+	});
+	
+	$( "#daily-small-orders-outlined" ).on( "click", function( event ) {
+		$("#Outlet-Section").delay(100).fadeIn();
+		$("#Whole-Form").delay(100).fadeIn();
+		$("#Others-Section").delay(100).fadeIn();
+		$("#Submit-Form-Button").delay(100).fadeIn();
+		
+		$("#Big-Order-Section").delay(100).fadeOut();
+		$("#Big-Order-Section").find('input[type=number]').val('');
+	});
+	
+	$( "#big-orders-outlined" ).on( "click", function( event ) {
+		$("#Outlet-Section").delay(100).fadeIn();
+		$("#Whole-Form").delay(100).fadeIn();
+		$("#Big-Order-Section").delay(100).fadeIn();
+		$("#Others-Section").delay(100).fadeIn();
+		$("#Submit-Form-Button").delay(100).fadeIn();
 	});
 	
 });
