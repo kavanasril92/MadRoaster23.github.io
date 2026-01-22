fdc6ad03e87f3c1b1c305cbf8da1b9559053978f
Kavan Lee <kavanasril@hotmail.com>
2026-01-22 14:05:59 +0800

    Update

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from pathlib import Path
 
 app = FastAPI()
 
-# PROJECT_DIR = Path(__file__).resolve()
-PROJECT_DIR = Path.cwd()
+PROJECT_DIR = Path(__file__).resolve().parent
+# PROJECT_DIR = Path.cwd()
 BACKEND_DIR = PROJECT_DIR.parent
-FRONTEND_DIR = PROJECT_DIR / "frontend"
+FRONTEND_DIR = BACKEND_DIR / "frontend"
+INDEX_FILE = FRONTEND_DIR / "index.html"
 
 print("CWD:", Path.cwd())
 print("PROJECT_ROOT:", PROJECT_DIR)
 print("FRONTEND_DIR exists:", FRONTEND_DIR.exists())
+print("INDEX_FILE:", INDEX_FILE)
+print("INDEX_FILE exists:", INDEX_FILE.exists())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 FRONTEND_DIR = BASE_DIR / "frontend"
@@ -28,7 +31,12 @@ if FRONTEND_DIR.exists():
 # Root → index.html
 @app.get("/")
 def read_index():
-    return FileResponse(FRONTEND_DIR / "index.html")
+    if not INDEX_FILE.exists():
+        return HTMLResponse("Frontend not found", status_code=404)
+
+    return FileResponse(INDEX_FILE)
+
+    # return FileResponse(FRONTEND_DIR / "index.html")
 
 
 # app.add_middleware(
